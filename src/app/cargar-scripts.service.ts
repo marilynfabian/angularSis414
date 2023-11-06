e5beab37fc01987ceb4043e1a99b9e90212efd4e
marilynfabian <marilynfabian27@gmail.com>
2023-11-06 08:25:42 -0400

    guardando

--- src/app/cargar-scripts.service.ts
+++ src/app/cargar-scripts.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class CargarScriptsService {
 
   constructor() { }
+
+  Carga(archivos:string[])
+  {
+     for(let archivo of archivos)
+     {
+      let script = document.createElement("script");
+      script.src = "./assets/js/" + archivo + ".js";
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+     } 
+  }
 }
