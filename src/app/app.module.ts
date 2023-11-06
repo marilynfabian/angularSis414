caea31a821c2808f4c6d1a5d53c21cdd9fe8823a
marilynfabian <marilynfabian27@gmail.com>
2023-11-06 07:58:05 -0400

    actualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    Test1Component,
+    
   ],
   imports: [
     BrowserModule,
